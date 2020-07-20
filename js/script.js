649efbdb34787bc9c103cab78ccfe0b11d11ec56
Vlasya <64101971+Vlasya@users.noreply.github.com>
2020-07-20 10:47:54 +0300

    Основная верстка

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var mySwiper = new Swiper('.swiper-container', {
+	// Optional parameters
+	loop: true,
+	slidesPerView:2,
+ 
+	// Navigation arrows
+	navigation: {
+	  nextEl: '.series-arrow',
+	},
+
+ })
